16439df5226f241fc9ef9b0ba7e67c0cfd7a6786
Vadim Filimonov <philimonovvadim@gmail.com>
2020-08-17 09:11:09 +0300

    fix linter warnings

--- src/js/change-city.js
+++ src/js/change-city.js
@@ -7,21 +7,21 @@ var searchFormInput = document.querySelector('.search-form__input');
 var searchFormButton = document.querySelector('.search-form__button');
 
 
-changeCityButton.addEventListener('click', function(evt) {
-    currentCityBlock.classList.add('visually-hidden');
-    searchFormBlock.classList.remove('visually-hidden');
-    evt.preventDefault();
+changeCityButton.addEventListener('click', function (evt) {
+	currentCityBlock.classList.add('visually-hidden');
+	searchFormBlock.classList.remove('visually-hidden');
+	evt.preventDefault();
 });
 
-myLocationButton.addEventListener('click', function(evt) {
-    evt.preventDefault();
+myLocationButton.addEventListener('click', function (evt) {
+	evt.preventDefault();
 });
 
-searchFormBlock.addEventListener('submit', function(evt) {
-    var city = searchFormInput.value;
-    searchFormInput.value = '';
-    currentCityTitle.textContent = city;
-    currentCityBlock.classList.remove('visually-hidden');
-    searchFormBlock.classList.add('visually-hidden');
-    evt.preventDefault();
+searchFormBlock.addEventListener('submit', function (evt) {
+	var city = searchFormInput.value;
+	searchFormInput.value = '';
+	currentCityTitle.textContent = city;
+	currentCityBlock.classList.remove('visually-hidden');
+	searchFormBlock.classList.add('visually-hidden');
+	evt.preventDefault();
 });
--- src/js/change-unit.js
+++ src/js/change-unit.js
@@ -2,24 +2,24 @@ var unitSwitcherRadios = document.querySelectorAll('.unit-switcher__radio');
 var currentTemperature = document.querySelector('.main-info__value span');
 
 var unitSwitcherClick = function (unitSwitcher) {
-    unitSwitcher.addEventListener('input', function() {
-        var temperature = currentTemperature.textContent;
-        if (unitSwitcher.id === 'unit-switcher-celsius') {
-            currentTemperature.textContent = Math.round(fromFahrenheitToCelsius(temperature));
-        } else {
-            currentTemperature.textContent = Math.round(fromCelsiusToFahrenheit(temperature));
-        }
-    });
+	unitSwitcher.addEventListener('input', function () {
+		var temperature = currentTemperature.textContent;
+		if (unitSwitcher.id === 'unit-switcher-celsius') {
+			currentTemperature.textContent = Math.round(fromFahrenheitToCelsius(temperature));
+		} else {
+			currentTemperature.textContent = Math.round(fromCelsiusToFahrenheit(temperature));
+		}
+	});
 }
 
 for (var i = 0; i < unitSwitcherRadios.length; i++) {
-    unitSwitcherClick(unitSwitcherRadios[i]);
+	unitSwitcherClick(unitSwitcherRadios[i]);
 }
 
 var fromCelsiusToFahrenheit = function (number) {
-    return (number * 9 / 5) + 32;
+	return (number * 9 / 5) + 32;
 }
 
 var fromFahrenheitToCelsius = function (number) {
-    return (number - 32) * 5 / 9;
+	return (number - 32) * 5 / 9;
 }
